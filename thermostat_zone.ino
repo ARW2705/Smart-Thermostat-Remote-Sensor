35eacc9693ae88383eecdc44e930f30f3c43bb78
Andrew Wanex <awanex2705@gmail.com>
2019-03-22 13:24:14 -0700

    refactor: extract login credentials to header file. add call to post confirmation if sensor post succeeded

--- thermostat_zone.ino
+++ thermostat_zone.ino
@@ -1,5 +1,5 @@
 /*======================================
- * 
+ 
   Remote Temperature/Humidity Sensor
   
   ESP8266
@@ -19,22 +19,29 @@
 const char* ROUTER_SSID = Authorization::ROUTER_SSID;
 const char* ROUTER_PASS = Authorization::ROUTER_PASS;
 const char* SERVER_ADDR = Authorization::SERVER_ADDR;
-const int HTTP_PORT = 2772;
-const int DEVICE_ID = 1;
-const int DHT_PIN = 0;
+const int HTTP_PORT = Authorization::SERVER_PORT;
+const int DEVICE_ID = 3822; // change for each device
+const int DHT_PIN = 2;
 const int DHT_TYPE = 22;
 const int DHT_MAX = 120;
-const int SENSOR_DELAY = 10 * 1000;
+const int SENSOR_DELAY = 5 * 1000;
 const int LONG_REPORT_DELAY = 2 * 60 * 1000;
 const int RAPID_REPORT_DELAY = 10 * 1000;
 const int INITIALIZE_T_H = -99;
 
 /* END SENSOR CONSTANT DEFINITIONS */
 
+
+/*========== Global Variable Definitions ==========*/
+bool DEBUG = true;
+
+/* END GLOBAL VARIABLE DEFINITIONS */
+
+
 /*========== Structure Definitions ==========*/
 struct System {
-  int id = -1;
-  bool isRapid = false;
+  int id = -1; // assigned by thermostat, not dependent on device id
+  bool isRapid = false; // true if this zone should be set to rapid tranmission
   int selectedDelay;
   float temperature = INITIALIZE_T_H;
   float humidity = INITIALIZE_T_H;
@@ -56,9 +63,10 @@ Timer timer;
 
 /* END OBJECT/STRUCTURE INITIALIZATIONS */
 
+
 /*========== Function Declarations ==========*/
 void getSensorData();
-void postSensorData();
+bool postSensorData();
 void postRapidConfirmation();
 bool isDHTPlausible(float, float);
 
@@ -66,14 +74,36 @@ bool isDHTPlausible(float, float);
 
 /* END DEFINITIONS */
 
-void setup() {
-  WiFi.mode(WIFI_STA);
 
+void setup() {
+  Serial.begin(9600);
+  if (!DEBUG) Serial.end();
+  Serial.setDebugOutput(DEBUG);
+  Serial.println();
+  delay(10);
+
+  
+  WiFi.mode(WIFI_STA);
+  WiFi.setSleepMode(WIFI_NONE_SLEEP);
+
+  Serial.print("Connecting to ");
+  Serial.print(ROUTER_SSID);
+  Serial.println("...\n");
   WiFi.begin(ROUTER_SSID, ROUTER_PASS);
+
+  int i = 0;
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
+    Serial.print(++i);
+    Serial.print(' ');
   }
+  Serial.print("Connected!");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+  Serial.println();
+  delay(10);
 
+  Serial.println("DHT sensor activated\n");
   dht.begin();
 
   timer.sensor = millis();
@@ -81,6 +111,8 @@ void setup() {
 
   sys.selectedDelay = RAPID_REPORT_DELAY;
 
+  Serial.println("Setup complete");
+  Serial.println("===========================================\n");
   delay(100);
 }
 
@@ -92,9 +124,15 @@ void loop() {
 
   if (millis() - timer.report > sys.selectedDelay) {
     if (isDHTPlausible(sys.temperature, sys.humidity)) {
-      postSensorData();
+      bool success = postSensorData();
+      if (success) {
+        postRapidConfirmation();
+      }
       timer.report = millis();
-    } 
+    } else {
+      timer.sensor = millis() - SENSOR_DELAY;
+      timer.report = millis() - sys.selectedDelay + 1000;
+    }
   }
 
   delay(100);
